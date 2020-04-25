246ce6b91b2d9094aaedae98d9d705a720f86802
Simone Bembi <simone.bembi@gmail.com>
2020-04-25 11:00:45 +0200

    Added deploy scripts

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -3,6 +3,7 @@ if (Test-Path -Path $demoDistPath) {
     Remove-Item -Recurse -Force $demoDistPath
 }
 yarn demo:build:production
+ssh do 'mkdir -p /home/liquid.particles'
 ssh do 'rm -rf /home/liquid.particles/*'
 scp -r $demoDistPath/* do:/home/liquid.particles
 ssh do 'chmod -R 777 /home/liquid.particles'
